9965c1589fa636f4657e7fd3c642680fe7dc544e
Nimira43 <n.rabone72@aol.com>
2024-10-23 21:38:30 +0100

    Using Semantic Ui for buttons demo

--- src/app/layouts/App.tsx
+++ src/app/layouts/App.tsx
@@ -1,7 +1,16 @@
+import { Button } from 'semantic-ui-react'
+
 function App() {
   return (
     <div>
-      <h1>eDiary</h1>
+      <div>
+        <h1>eDiary</h1>
+        <button className='ui icon red button'> 
+          <i className="user icon"></i>
+          Button
+        </button>
+        <Button icon='smile' content='Button' color='blue'/>
+      </div>
     </div>
   )
 }
