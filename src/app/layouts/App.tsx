56ea85a8014d07edd9692d8ebeb13dba70408abc
Nimira43 <n.rabone72@aol.com>
2024-10-23 21:50:49 +0100

    Creating EventDashboard

--- src/app/layouts/App.tsx
+++ src/app/layouts/App.tsx
@@ -1,15 +1,11 @@
-import { Button } from 'semantic-ui-react'
+import EventDashboard from '../../features/events/dashboard/EventDashboard'
 
 function App() {
   return (
     <div>
       <div>
         <h1>eDiary</h1>
-        <button className='ui icon red button'> 
-          <i className="user icon"></i>
-          Button
-        </button>
-        <Button icon='smile' content='Button' color='blue'/>
+        <EventDashboard />
       </div>
     </div>
   )
