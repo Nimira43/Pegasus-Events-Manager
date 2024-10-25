0b2f1ddac43cd17e03b769091782a7821bff7142
Nimira43 <n.rabone72@aol.com>
2024-10-25 19:56:35 +0100

    Expanding on Event List cards

--- src/features/events/dashboard/EventListItem.tsx
+++ src/features/events/dashboard/EventListItem.tsx
@@ -1,4 +1,5 @@
-import { Item, ItemGroup, Segment, SegmentGroup } from 'semantic-ui-react'
+import EventListAttendee from './EventListAttendee'
+import { Button, Icon, Item, ItemGroup, List, Segment, SegmentGroup } from 'semantic-ui-react'
 
 export default function EventListItem() {
   return (
@@ -7,8 +8,31 @@ export default function EventListItem() {
         <ItemGroup>
           <Item>
             <Item.Image size='tiny' circular src='/user.png'/>
+            <Item.Content>
+              <Item.Header>Event Title</Item.Header>
+              <Item.Description>
+                Hosted by NimiraTech
+              </Item.Description>
+            </Item.Content>          
           </Item>
         </ItemGroup>
+      </Segment>
+      <Segment>
+        <span>
+          <Icon name='clock' /> Date
+          <Icon name='marker' /> Venue
+        </span>
+      </Segment>
+      <Segment secondary>
+        <List horizonal>
+          <EventListAttendee />
+          <EventListAttendee />
+          <EventListAttendee />
+        </List>
+      </Segment>
+      <Segment clearing>
+        <span>Description of the event</span>
+        <Button color='black' floated='right' />
       </Segment>
     </SegmentGroup>
   )
