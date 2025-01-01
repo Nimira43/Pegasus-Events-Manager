c30a1fd0c94b61af420ec09db9a91c0bc83e578e
Nimira43 <n.rabone72@aol.com>
2025-01-01 23:15:11 +0000

    Created API folder and brought in sample data

---
+++ src/app/api/sampleData.ts
@@ -0,0 +1,53 @@
+export const sampleData = [
+  {
+    id: '1',
+    title: 'Birthday party for Megan',
+    date: '2025-01-19',
+    category: 'culture',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, illum explicabo enim suscipit ab nisi molestiae porro recusandae libero voluptatum facere deserunt omnis!',
+    city: 'Manchester, UK',
+    venue: 'Vanessa Dobson, 29 Romford Road, Manchester, UK',
+    hostedBy: 'Vanessa',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/women/82.jpg',
+    attendees: [
+        {
+            id: 'a',
+            name: 'Megan',
+            photoURL: 'https://randomuser.me/api/portraits/women/11.jpg'
+        },
+        {
+            id: 'b',
+            name: 'Danny',
+            photoURL: 'https://randomuser.me/api/portraits/men/73.jpg'
+        },
+        {
+            id: 'c',
+            name: 'Phil',
+            photoURL: 'https://randomuser.me/api/portraits/men/38.jpg'
+        }
+    ]
+},
+{
+    id: '2',
+    title: 'Darts at the Fox and Goose',
+    date: '2025-02-21',
+    category: 'drinks',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. In optio fugiat nesciunt illo natus dolore et numquam distinctio eveniet, impedit quam aliquam. Ea, cumque laudantium.',
+    city: 'Birmingham, UK',
+    venue: 'Fox and Goose, 23 MainStreet, Birmingham, UK',
+    hostedBy: 'Mick',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/59.jpg',
+    attendees: [
+      {
+        id: 'a',
+        name: 'Eddie',
+        photoURL: 'https://randomuser.me/api/portraits/men/61.jpg'
+      },
+      {
+        id: 'b',
+        name: 'Debbie',
+        photoURL: 'https://randomuser.me/api/portraits/women/14.jpg'
+      }
+    ]
+  }
+];
