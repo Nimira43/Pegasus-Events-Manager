5b272c0c67c7a2d133cdb9bd04123e4ce941ad0c
Nimira43 <n.rabone72@aol.com>
2025-01-01 18:07:02 +0000

    Soft reboot of project

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../src/app/layouts/styles.css'
-import App from './app/layouts/App.tsx'
+import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
