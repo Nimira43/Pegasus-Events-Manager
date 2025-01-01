bec58ebe220250f2ea003b6f6cd2ac80f234e6f3
Nimira43 <n.rabone72@aol.com>
2025-01-01 20:46:59 +0000

    Styling nav Buttons. Also adding custom classes to overide some of Semantic UI's defaults.

--- src/app/layout/nav/NavBar.tsx
+++ src/app/layout/nav/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Container, Menu, MenuItem } from 'semantic-ui-react'
+import { Button, Container, Menu, MenuItem } from 'semantic-ui-react'
 
 export default function NavBar() {
   return (
@@ -7,6 +7,22 @@ export default function NavBar() {
           <MenuItem header>
             <img src='/logo1.png' alt='logo'></img>
             <span className='logo'>Pegasus</span>
+          </MenuItem>
+          <MenuItem>
+            <span className='heading'>Events</span>
+          </MenuItem>
+          <MenuItem >
+            <Button floated='right' basic inverted color='teal' >
+              <span className='btn'>Create Event</span>
+            </Button>
+          </MenuItem>
+          <MenuItem position='right'>
+            <Button basic inverted color='teal'>
+              <span className='btn'>Login</span>
+            </Button>
+            <Button basic inverted color='teal' style={{marginLeft: '0.5em'}}>
+              <span className='btn'>Register</span>
+            </Button>
           </MenuItem>
         </Container>  
       </Menu>
