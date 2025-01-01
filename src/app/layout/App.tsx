340a87664fb1c800afe7b87e2541265f58f1abfe
Nimira43 <n.rabone72@aol.com>
2025-01-01 18:52:17 +0000

    Using Semantic UI components for testing - all good

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,9 +1,21 @@
+import { Button } from 'semantic-ui-react'
+
 function App() {
   return (
     <>
       <h1 className='logo'>Pegasus</h1>
       <hr />
       <p>Events Manager</p>
+        <Button
+          icon='user'
+          content='Login' 
+          color='orange'
+        />
+        <Button
+          icon='user'
+          content='Logout' 
+          color='black'
+        />
     </>
   )
 }
