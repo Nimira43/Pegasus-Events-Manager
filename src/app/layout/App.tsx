136f182d9a632150049f9647d8972cf6f30d4fa2
Nimira43 <n.rabone72@aol.com>
2025-01-30 22:12:35 +0000

    Starting to link up the Create Event button in the navbar to the Create Event form.

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,13 +1,18 @@
 import { Container } from 'semantic-ui-react'
 import EventDashboard from '../../features/events/dashboard/EventDashboard'
 import NavBar from './nav/NavBar'
+import { useState } from 'react'
 
 function App() {
+  const [formOpen, setFormOpen] = useState(false)
+  
   return (
+
+
     <>
       <NavBar />
       <Container className='main'>
-        <EventDashboard />
+        <EventDashboard formOpen={formOpen} />
       </Container>
     </>
   )
--- src/features/events/dashboard/EventDashboard.tsx
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -3,14 +3,19 @@ import EventList from './EventList'
 import EventForm from '../form/EventForm'
 import { sampleData } from '../../../app/api/sampleData'
 
-export default function EventDashboard() {
+type Props = {
+  formOpen: boolean
+}
+
+export default function EventDashboard({formOpen}: Props) {
   return (
     <Grid>
       <Grid.Column width={10}>
         <EventList events={sampleData}/>
       </Grid.Column>
       <Grid.Column width={6}>
-        <EventForm />
+        {formOpen && 
+        <EventForm />}
       </Grid.Column>
     </Grid>      
   )
