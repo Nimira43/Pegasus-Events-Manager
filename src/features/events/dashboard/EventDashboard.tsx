c06bb88a6733408dd8db3798e0618f04754bc5c5
Nimira43 <n.rabone72@aol.com>
2025-02-16 00:56:16 +0000

    Created state variable events

--- src/features/events/dashboard/EventDashboard.tsx
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -2,17 +2,21 @@ import { Grid } from 'semantic-ui-react'
 import EventList from './EventList'
 import EventForm from '../form/EventForm'
 import { sampleData } from '../../../app/api/sampleData'
+import { useState } from 'react'
+import { AppEvent } from '../../../app/types/events'
 
 type Props = {
   formOpen: boolean
   setFormOpen: (value: boolean) => void
 }
 
-export default function EventDashboard({formOpen, setFormOpen}: Props) {
+export default function EventDashboard({ formOpen, setFormOpen }: Props) {
+  const [events, setEvents] = useState<AppEvent>([])
+
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList events={sampleData}/>
+        <EventList events={events}/>
       </Grid.Column>
       <Grid.Column width={6}>
         {formOpen && 
