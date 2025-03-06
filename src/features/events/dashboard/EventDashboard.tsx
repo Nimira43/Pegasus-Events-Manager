3a33a16dfb542a15ddb348517f05aa313bf3ce3f
Nimira43 <n.rabone72@aol.com>
2025-03-06 00:33:08 +0000

    Applying null or AppEvent type for the new state variables

--- src/features/events/dashboard/EventDashboard.tsx
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 export default function EventDashboard({ formOpen, setFormOpen }: Props) {
   const [events, setEvents] = useState<AppEvent[]>([])
-  const [selectedEvent, setSelectedEvent] = useState(null)
+  const [selectedEvent, setSelectedEvent] = useState<AppEvent | null>(null)
 
   useEffect(() => {
     setEvents(sampleData)
