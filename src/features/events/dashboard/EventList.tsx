69c0c69a04e144d00fe3aa74d756f551828fdece
Nimira43 <n.rabone72@aol.com>
2025-03-12 02:37:21 +0000

    Added deleteEvent property.

--- src/features/events/dashboard/EventList.tsx
+++ src/features/events/dashboard/EventList.tsx
@@ -19,6 +19,7 @@ export default function EventList({
           key={event.id}
           event={event}
           selectEvent={selectEvent}
+          deleteEvent={deleteEvent}
         />
       ))}
     </>
