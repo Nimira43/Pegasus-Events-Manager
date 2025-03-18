59ec9ba7e4128a27c0a98f776e58688fff9151ad
Nimira43 <n.rabone72@aol.com>
2025-03-18 23:13:55 +0000

    Brought in useLocation. If on / then the HomePage is displayed otherwise it'll be the Outlet.

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,13 +1,16 @@
 import { Container } from 'semantic-ui-react'
 import NavBar from './nav/NavBar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import HomePage from '../../features/home/HomePage'
 
 function App() {
+  const location = useLocation()
+
   return (
     <>
       <NavBar />
       <Container className='main'>
-        <Outlet />
+        {location.pathname === '/' ? <HomePage /> : <Outlet />}
       </Container>
     </>
   )
