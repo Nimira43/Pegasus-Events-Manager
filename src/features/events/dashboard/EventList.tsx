a0efb45fa61cd11e82635817879e759245ee1f1c
Nimira43 <n.rabone72@aol.com>
2025-03-18 22:12:42 +0000

    Removed select and delete event functions from EventList

--- src/features/events/dashboard/EventList.tsx
+++ src/features/events/dashboard/EventList.tsx
@@ -3,14 +3,10 @@ import EventListItem from './EventListItem'
 
 type Props = {
   events: AppEvent[]
-  selectEvent: (event: AppEvent) => void
-  deleteEvent: (eventId: string) => void
 }
 
 export default function EventList({
   events,
-  selectEvent,
-  deleteEvent
 }: Props) {
   return (
     <>
@@ -18,8 +14,6 @@ export default function EventList({
         <EventListItem
           key={event.id}
           event={event}
-          selectEvent={selectEvent}
-          deleteEvent={deleteEvent}
         />
       ))}
     </>
