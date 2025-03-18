99b7f7916dbfeee5b289571b3fdaa50b1a088069
Nimira43 <n.rabone72@aol.com>
2025-03-18 21:47:43 +0000

    Imported RouterProvider into main.tsx and passed in the newly created router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/layout/styles.css'
 import App from './app/layout/App'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/router/Routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RouterProvider router={router}/>
     <App />
   </StrictMode>,
 )
