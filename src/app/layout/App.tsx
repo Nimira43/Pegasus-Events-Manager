ac95a745a4146960591f318c8aad252165af83f5
Nimira43 <n.rabone72@aol.com>
2025-03-18 21:58:39 +0000

    Cleaned out unwanted code from App.tsx

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,26 +1,13 @@
 import { Container } from 'semantic-ui-react'
 import NavBar from './nav/NavBar'
-import { useState } from 'react'
-import { AppEvent } from '../types/events'
 import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [formOpen, setFormOpen] = useState(false)
-  const [selectedEvent, setSelectedEvent] = useState<AppEvent | null>(null)
-
-  function handleSelectEvent(event: AppEvent | null) {
-    setSelectedEvent(event)
-    setFormOpen(true)
-  }
-  
-  function handleCreatedFormOpen() {
-    setSelectedEvent(null)
-    setFormOpen(true)
-  }
+ 
 
   return (
     <>
-      <NavBar setFormOpen={handleCreatedFormOpen}/>
+      <NavBar />
       <Container className='main'>
         <Outlet />
       </Container>
