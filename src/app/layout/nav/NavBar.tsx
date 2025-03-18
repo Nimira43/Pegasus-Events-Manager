8794a0deb5cd8062be555d2bc6d02df70882bb3e
Nimira43 <n.rabone72@aol.com>
2025-03-18 23:05:02 +0000

    Reverting back some of the styling previously made. Added more NavLinks

--- src/app/layout/nav/NavBar.tsx
+++ src/app/layout/nav/NavBar.tsx
@@ -4,19 +4,26 @@ import { Button, Container, Menu, MenuItem } from 'semantic-ui-react'
 export default function NavBar() {
   return (
       <Menu inverted={true} fixed='top'>
-        <Container className='navbar'>
-          <MenuItem header as={NavLink} to='/'>
+        <Container>
+          <MenuItem
+            header
+            as={NavLink}
+            to='/'
+          >
             <img src='/logo1.png' alt='logo'></img>
             <span className='logo'>Pegasus</span>
           </MenuItem>
           <MenuItem
-            as={NavLink} to='/events'
+            as={NavLink}
+            to='/events'
             className='heading'
           >
             <span className='heading'>Events</span>
           </MenuItem>
-          <MenuItem >
+          <MenuItem>
             <Button 
+              as={NavLink}
+              to='/createEvent'
               floated='right' 
               basic 
               inverted 
