e7cc57267c94ad92a3dfed094bd9b5999cc17d1c
Nimira43 <n.rabone72@aol.com>
2025-04-04 03:46:58 +0100

    Starting to work on the HomePage

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,5 +1,28 @@
+import { Container, Header, Image, Segment } from 'semantic-ui-react'
+
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <Segment 
+      inverted
+      textAlign='center'
+      vertical
+      className='masthead'
+    >
+      <Container>
+        <Header as='h1' inverted>
+          <Image
+            size='massive'
+            src='/logo1.png'
+            alt='logo'
+            style={{
+              marginBottom: 12
+            }}
+          />
+          <span className='logo'>Pegasus</span>
+          
+        </Header>
+      </Container>
+    </Segment>
+
   )
 }
