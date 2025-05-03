a552d1218a96cd6b27591c2f2cb3dc29bf9da61f
Nimira43 <n.rabone72@aol.com>
2025-05-03 20:56:18 +0100

    Working on signed in menu

--- src/app/layout/nav/SignedInMenu.tsx
+++ src/app/layout/nav/SignedInMenu.tsx
@@ -1,5 +1,12 @@
+import { Dropdown, Image, Menu } from 'semantic-ui-react'
+
 export default function SignedInMenu() {
   return (
-    <div>SignedInMenu</div>
+    <Menu.Item position='right'>
+      <Image avatar spaced='right' src='/user.png' />
+      <Dropdown pointing='top left' text='Lenny'>
+        <Dropdown.Item />
+      </Dropdown>
+    </Menu.Item>
   )
 }
