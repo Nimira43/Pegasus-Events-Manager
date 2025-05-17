bc38d75d05ea350abb10aed1790bb3d43d59d910
Nimira43 <n.rabone72@aol.com>
2025-05-17 02:47:35 +0100

    Setting conditional for when SignedInMenu / SignedOutButtons are displayed.

--- src/app/layout/nav/NavBar.tsx
+++ src/app/layout/nav/NavBar.tsx
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom'
 import { Button, Container, Menu, MenuItem } from 'semantic-ui-react'
-// import SignedOutButtons from './SignedOutButtons'
+import SignedOutButtons from './SignedOutButtons'
 import SignedInMenu from './SignedInMenu'
 import { useState } from 'react'
 
@@ -8,37 +8,41 @@ export default function NavBar() {
   const [auth, setAuth] = useState(true)
 
   return (
-      <Menu inverted={true} fixed='top'>
-        <Container>
-          <MenuItem
-            header
+    <Menu inverted={true} fixed='top'>
+      <Container>
+        <MenuItem
+          header
+          as={NavLink}
+          to='/'
+        >
+          <img src='/logo1.png' alt='logo'></img>
+          <span className='logo'>Pegasus</span>
+        </MenuItem>
+        <MenuItem
+          as={NavLink}
+          to='/events'
+          className='heading'
+        >
+          <span className='heading'>Events</span>
+        </MenuItem>
+        <MenuItem>
+          <Button 
             as={NavLink}
-            to='/'
-          >
-            <img src='/logo1.png' alt='logo'></img>
-            <span className='logo'>Pegasus</span>
-          </MenuItem>
-          <MenuItem
-            as={NavLink}
-            to='/events'
-            className='heading'
-          >
-            <span className='heading'>Events</span>
-          </MenuItem>
-          <MenuItem>
-            <Button 
-              as={NavLink}
-              to='/createEvent'
-              floated='right' 
-              basic 
-              inverted 
-              color='teal'>
-              <span className='btn'>Create Event</span>
-            </Button>
-          </MenuItem>
-        {/* <SignedOutButtons /> */}
-        <SignedInMenu />
-        </Container>  
-      </Menu>
+            to='/createEvent'
+            floated='right' 
+            basic 
+            inverted 
+            color='teal'>
+            <span className='btn'>Create Event</span>
+          </Button>
+        </MenuItem>
+        {auth
+          ?
+          <SignedInMenu setAuth={setAuth} />
+          :
+          <SignedOutButtons setAuth={setAuth} />
+        }
+      </Container>  
+    </Menu>
   )
 }
