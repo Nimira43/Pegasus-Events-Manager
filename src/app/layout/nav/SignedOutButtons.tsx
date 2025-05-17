bfa43deb94b950371dbb9bc6c7910e3e5d101f48
Nimira43 <n.rabone72@aol.com>
2025-05-17 02:56:51 +0100

    Signing out working

--- src/app/layout/nav/SignedOutButtons.tsx
+++ src/app/layout/nav/SignedOutButtons.tsx
@@ -1,10 +1,16 @@
 import { MenuItem, Button } from 'semantic-ui-react'
 
+type Props = {
+  setAuth: (value: boolean) => void
+}
 
-export default function SignedOutButtons() {
+export default function SignedOutButtons({setAuth}: Props) {
   return (
     <MenuItem position='right'>
-      <Button basic inverted color='teal'>
+      <Button
+        basic inverted color='teal'
+        onClick={() => setAuth(true)}
+      >
         <span className='btn'>Login</span>
       </Button>
       <Button basic inverted color='teal' style={{marginLeft: '0.5em'}}>
