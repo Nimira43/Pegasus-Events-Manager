88a616f4b2eed76aa04ad4465e23b5cf636fcb74
Nimira43 <n.rabone72@aol.com>
2025-06-29 16:33:33 +0100

    Bringing in testSlice into the store.

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { testSlice } from '../scratch/testSlice'
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    test: testSlice.reducer
+  }
 })
 
 export type RootState = ReturnType<typeof store.getState>
