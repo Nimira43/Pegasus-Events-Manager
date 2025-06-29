d0f0c6b0fa920db326c02fb6fcd98107c832699f
Nimira43 <n.rabone72@aol.com>
2025-06-29 16:37:35 +0100

    Created data object variable using useAppSelector

--- src/app/scratch/Scratch.tsx
+++ src/app/scratch/Scratch.tsx
@@ -1,8 +1,13 @@
+import { useAppSelector } from "../store/store"
+
 export default function Scratch() {
+  const {data} = useAppSelector(state => state.test)
+
+
   return (
     <div>
       <h1>Redux Testing Page</h1>
-      <h3>Data:</h3>
+      <h3>Data: {data}</h3>
     </div>
   )
 }
