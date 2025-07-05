d728ff80495f95ce262fe25b7d68b43cb12593f8
Nimira43 <n.rabone72@aol.com>
2025-07-05 22:53:46 +0100

    Created Button - emply at the moment.

--- src/app/scratch/Scratch.tsx
+++ src/app/scratch/Scratch.tsx
@@ -1,13 +1,15 @@
+import { Button } from 'semantic-ui-react'
 import { useAppSelector } from '../store/store'
 
 export default function Scratch() {
   const {data} = useAppSelector(state => state.test)
-
+  
 
   return (
     <div>
       <h1>Redux Testing Page</h1>
       <h3>Data: {data}</h3>
+      <Button onClick={()} />
     </div>
   )
 }
