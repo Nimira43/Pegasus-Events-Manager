28fe6f3d879a5877bfe96426bec45923ca9ccad8
Nimira43 <n.rabone72@aol.com>
2025-10-11 23:24:59 +0100

    ModalManager - 4

--- src/common/modals/ModalManager.tsx
+++ src/common/modals/ModalManager.tsx
@@ -8,7 +8,14 @@ export default function ModalManager() {
 
   const {type, data, open} = useAppSelector(state => state.modals)
 
+  let renderedModal
+
+  if (open && type) {
+    const ModalComponent = (modalLookup as any)[type]
+    renderedModal = <ModalComponent data={data} />
+  }
+
   return (
-    <div>Modal Manager</div>
+    <span>{renderedModal}</span>
   )
 }
