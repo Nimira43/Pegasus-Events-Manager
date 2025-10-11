48ff1849569524e8d2644e31ee5127bc16db1d42
Nimira43 <n.rabone72@aol.com>
2025-10-11 22:56:40 +0100

    ModalManager - 2

---
+++ src/common/modals/ModalManager.tsx
@@ -0,0 +1,13 @@
+import TestModal from '../../app/scratch/TestModal'
+
+export default function ModalManager() {
+  const modalLookup = {
+    TestModal
+  }
+
+  
+
+  return (
+    <div>Modal Manager</div>
+  )
+}
