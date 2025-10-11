cc9d9be75bd2e31feec11e6e570d2e4b06c3591e
Nimira43 <n.rabone72@aol.com>
2025-10-11 22:57:59 +0100

    ModalManager - 3

--- src/common/modals/ModalManager.tsx
+++ src/common/modals/ModalManager.tsx
@@ -1,11 +1,12 @@
 import TestModal from '../../app/scratch/TestModal'
+import { useAppSelector } from '../../app/store/store'
 
 export default function ModalManager() {
   const modalLookup = {
     TestModal
   }
 
-  
+  const {type, data, open} = useAppSelector(state => state.modals)
 
   return (
     <div>Modal Manager</div>
