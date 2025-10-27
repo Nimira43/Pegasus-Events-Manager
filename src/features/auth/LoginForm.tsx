8b37904b7678dd008f87adb63d8b758651beac34
Nimira43 <n.rabone72@aol.com>
2025-10-27 00:22:43 +0000

    LoginForm - 1

---
+++ src/features/auth/LoginForm.tsx
@@ -0,0 +1,13 @@
+import { Form } from 'semantic-ui-react'
+import ModalWrapper from '../../common/modals/ModalWrapper'
+
+export default function LoginForm() {
+  return (
+    <ModalWrapper header='Login to Pegasus'
+    >
+      <Form>
+        
+      </Form>
+    </ModalWrapper>
+  )
+}
