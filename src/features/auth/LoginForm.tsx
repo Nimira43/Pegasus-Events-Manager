b95c4d895b6e4b326210c71ef66149da2e6f086d
Nimira43 <n.rabone72@aol.com>
2025-10-27 00:25:35 +0000

    LoginForm - 2

--- src/features/auth/LoginForm.tsx
+++ src/features/auth/LoginForm.tsx
@@ -2,11 +2,24 @@ import { Form } from 'semantic-ui-react'
 import ModalWrapper from '../../common/modals/ModalWrapper'
 
 export default function LoginForm() {
+  const {
+    register, 
+    handleSubmit
+    formState: {
+      isSubmitting,
+      isValid,
+      isDirty,
+      errors
+    }
+  } = useForm({
+    mode: 'onTouched'
+  })
+
   return (
     <ModalWrapper header='Login to Pegasus'
     >
       <Form>
-        
+
       </Form>
     </ModalWrapper>
   )
