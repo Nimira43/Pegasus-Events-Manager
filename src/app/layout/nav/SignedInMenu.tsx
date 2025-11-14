866cb9dde191348aa1b2291eccff179da93f564d
Nimira43 <n.rabone72@aol.com>
2025-11-14 23:16:48 +0000

    Also bringing in dispatch from store

--- src/app/layout/nav/SignedInMenu.tsx
+++ src/app/layout/nav/SignedInMenu.tsx
@@ -1,10 +1,10 @@
 import { Dropdown, Image, Menu } from 'semantic-ui-react'
 import { Link, useNavigate } from 'react-router-dom'
-import { useAppSelector } from '../../store/store'
+import { useAppDispatch, useAppSelector } from '../../store/store'
 
 export default function SignedInMenu() {
   const {currentUser} = useAppSelector(state => state.auth)
-
+  const dispatch = useAppDispatch()
   const navigate = useNavigate()
 
   function handleSignOut() {
