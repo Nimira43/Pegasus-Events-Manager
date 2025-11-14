3c1de9f6320280f5bccfe2cdc4f1ce0239d23448
Nimira43 <n.rabone72@aol.com>
2025-11-14 22:53:03 +0000

    Remoed auth state variable and removed the useState import

--- src/app/layout/nav/NavBar.tsx
+++ src/app/layout/nav/NavBar.tsx
@@ -2,10 +2,8 @@ import { NavLink } from 'react-router-dom'
 import { Button, Container, Menu, MenuItem } from 'semantic-ui-react'
 import SignedOutButtons from './SignedOutButtons'
 import SignedInMenu from './SignedInMenu'
-import { useState } from 'react'
 
 export default function NavBar() {
-  const [auth, setAuth] = useState(false)
 
   return (
     <Menu inverted={true} fixed='top'>
