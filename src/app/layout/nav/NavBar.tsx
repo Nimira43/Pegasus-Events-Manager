4d303ea8d43d1cc85193b0611cc876e43bbe3ab1
Nimira43 <n.rabone72@aol.com>
2025-11-14 22:56:21 +0000

    Extracts authenticated value from Redux auth state using selector.

--- src/app/layout/nav/NavBar.tsx
+++ src/app/layout/nav/NavBar.tsx
@@ -2,9 +2,10 @@ import { NavLink } from 'react-router-dom'
 import { Button, Container, Menu, MenuItem } from 'semantic-ui-react'
 import SignedOutButtons from './SignedOutButtons'
 import SignedInMenu from './SignedInMenu'
+import { useAppSelector } from '../../store/store'
 
 export default function NavBar() {
-
+  const { authenticated } = useAppSelector(state => state.auth)
   return (
     <Menu inverted={true} fixed='top'>
       <Container>
